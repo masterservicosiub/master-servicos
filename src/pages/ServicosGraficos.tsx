65eaea204de9a8d602ec9e8ac6ee95486e71b21c
masterservicosiub <masterservicos.iub@gmail.com>
2026-05-05 23:05:45 -0300

    Update ServicosGraficos.tsx

--- src/pages/ServicosGraficos.tsx
+++ src/pages/ServicosGraficos.tsx
@@ -1,9 +1,22 @@
 import Orcamento from "./Orcamento";
+import logoGrafica from "./assets/logo-master-grafica.png"; // ajuste o caminho da imagem
 
 const ServicosGraficos = () => (
   <Orcamento
     kind="grafico"
-    pageTitle="Serviços Gráficos"
+    pageTitle={
+      <img
+        src={logoGrafica}
+        alt="Master Gráfica"
+        style={{
+          maxWidth: "280px",
+          width: "100%",
+          height: "auto",
+          display: "block",
+          margin: "0 auto"
+        }}
+      />
+    }
     pageSubtitle="Solicite cartões, banners, panfletos e outros materiais gráficos."
   />
 );
